9eba660bb28e08972f43483b63158d30f89ec46c
Leonardo Celis <leonardocelis112@gmail.com>
2020-04-30 10:36:48 -0500

    Add videos container

--- trainer/src/app/app.module.ts
+++ trainer/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { WorkoutRunnerModule } from "./workout-runner/workout-runner.module";
 import { ExerciseDescriptionComponent } from "./workout-runner/exercise-description/exercise-description.component";
+import { VideoPlayerComponent } from "./workout-runner/exercise-description/video-player/video-player.component";
 
 @NgModule({
   declarations: [AppComponent],
--- trainer/src/app/workout-runner/workout-runner.module.ts
+++ trainer/src/app/workout-runner/workout-runner.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { WorkoutRunnerComponent } from "./workout-runner.component";
 import { ExerciseDescriptionComponent } from "./exercise-description/exercise-description.component";
+import { VideoPlayerComponent } from "./exercise-description/video-player/video-player.component";
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent],
+  declarations: [
+    WorkoutRunnerComponent,
+    ExerciseDescriptionComponent,
+    VideoPlayerComponent,
+  ],
   exports: [WorkoutRunnerComponent],
 })
 export class WorkoutRunnerModule {}
