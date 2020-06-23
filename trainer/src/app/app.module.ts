c72201a78ec6de43b16ad1debe45ae3207386bc0
Leonardo Celis <leonardocelis112@gmail.com>
2020-06-22 21:54:10 -0500

    Add navigation

--- trainer/src/app/app.module.ts
+++ trainer/src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { WorkoutRunnerModule } from "./workout-runner/workout-runner.module";
-import { ExerciseDescriptionComponent } from "./workout-runner/exercise-description/exercise-description.component";
-import { VideoPlayerComponent } from "./workout-runner/exercise-description/video-player/video-player.component";
+import { AppComponent } from './app.component';
+import { WorkoutRunnerModule } from './workout-runner/workout-runner.module';
+import { ExerciseDescriptionComponent } from './workout-runner/exercise-description/exercise-description.component';
+import { VideoPlayerComponent } from './workout-runner/exercise-description/video-player/video-player.component';
+import { StartComponent } from './start/start.component';
+import { FinishComponent } from './finish/finish.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, WorkoutRunnerModule],
+  declarations: [AppComponent, StartComponent, FinishComponent],
+  imports: [BrowserModule, WorkoutRunnerModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
